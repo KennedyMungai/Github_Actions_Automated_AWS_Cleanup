9047cbbd7478a3c52ac5e534fe95c7e865b03261
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-09-15 19:59:20 +0300

    Used the Force flag instead of the WhatIf flag

--- aws-cleanup-ec2.ps1
+++ aws-cleanup-ec2.ps1
@@ -1,2 +1,2 @@
 # Command for clearing the EC2 instance jobs
-(Get-EC2Instance).Instances | ForEach-Object { Remove-EC2Instance -InstanceId $PSItem.InstanceId -WhatIf }
+(Get-EC2Instance).Instances | ForEach-Object { Remove-EC2Instance -InstanceId $PSItem.InstanceId -Force }
