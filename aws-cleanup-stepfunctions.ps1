22a72c9557962df786b47d506c3866d07b23f2ac
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-09-15 16:13:04 +0300

    Added some comments to the script

--- aws-cleanup-stepfunctions.ps1
+++ aws-cleanup-stepfunctions.ps1
@@ -1,3 +1,5 @@
+# Cleanup State Machines
 Get-SFNStateMachineList | ForEach-Object { Remove-SFNStateMachine -StateMachineArn $PSItem.StateMachineArn -WhatIf }
 
+# Cleanup Activity Tasks
 Get-SFNActivityList | ForEach-Object { Remove-SFNActivity -ActivityArn $PSItem.ActivityArn -WhatIf }
