327cf92c538c37bce32a0b4607ea2bc262e9fc46
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-09-15 19:12:22 +0300

    Added the Force flag for automatic confirmation

--- aws-cleanup-stepfunctions.ps1
+++ aws-cleanup-stepfunctions.ps1
@@ -1,5 +1,5 @@
 # Cleanup State Machines
-Get-SFNStateMachineList | ForEach-Object { Remove-SFNStateMachine -StateMachineArn $PSItem.StateMachineArn }
+Get-SFNStateMachineList | ForEach-Object { Remove-SFNStateMachine -StateMachineArn $PSItem.StateMachineArn -Force }
 
 # Cleanup Activity Tasks
-Get-SFNActivityList | ForEach-Object { Remove-SFNActivity -ActivityArn $PSItem.ActivityArn }
+Get-SFNActivityList | ForEach-Object { Remove-SFNActivity -ActivityArn $PSItem.ActivityArn -Force }
